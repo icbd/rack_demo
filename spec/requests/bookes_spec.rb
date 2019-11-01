45bf6df04654ab42d3febb2225fe4bf28645d822
CbdFocus <wwwicbd@gmail.com>
2019-11-01 18:50:50 +0800

    FEATURE - Add RSpec

---
+++ spec/requests/bookes_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../spec_helper'
+
+RSpec.describe 'Books', type: :request do
+  let(:server) { Server.new }
+
+  it 'GET /books' do
+    env = { 'REQUEST_METHOD' => 'GET', 'REQUEST_PATH' => '/books' }
+    response = server.call(env)
+
+    expect(response[0]).to eq 200
+    expect(response[2][0]).to eq Book.all.order(:id).to_json
+  end
+
+  it 'GET /books/{ISBN}' do
+    book = Book.last
+
+    env = { 'REQUEST_METHOD' => 'GET', 'REQUEST_PATH' => "/books/#{book.isbn}" }
+    response = server.call(env)
+
+    expect(response[0]).to eq 200
+    expect(response[2][0]).to eq book.to_json
+  end
+
+  it 'GET /error_routes' do
+    env = { 'REQUEST_METHOD' => 'GET', 'REQUEST_PATH' => '/error_routes' }
+    response = server.call(env)
+
+    expect(response[0]).to eq 404
+    expect(response[2][0]).to eq 'Routing Error'
+  end
+end
